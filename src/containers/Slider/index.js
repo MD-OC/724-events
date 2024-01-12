208bcf44dc567680e55fc0fde3315001c5f7eed0
MD-OC <registernewservice@gmail.com>
2024-01-12 14:43:42 +0000

    Corrige l'affichage de l'image blanche dans le slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,9 +10,10 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
+  const slides = data?.focus.length
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < slides -1 ? index + 1 : 0),
       5000
     );
   };
